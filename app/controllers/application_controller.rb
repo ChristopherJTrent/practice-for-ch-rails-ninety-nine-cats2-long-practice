0c2a111683178d2889d967983f4de0646ac7a2e4
acheung-94 <acheung@ucdavis.edu>
2024-02-19 14:15:34 -0800

    implement CRRLLL methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in
+
+    def current_user
+        User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        redirect_to new_session_url unless user.logged_in?
+    end
+
+    def require_logged_out
+        redirect_to "/" if user.logged_in?
+    end
+
+    def log_in(user)
+        token = user.reset_session_token
+        session[:session_token] = token
+        redirect_to :root
+    end
+
+    def log_out
+        user.reset_session_token
+        session[:session_token] = nil
+        redirect_to :root
+    end
+
+    def logged_in?
+        !!current_user
+        #session[:session_token] == user.session_token
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# Example:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+ActiveRecord::Base.connection.reset_pk_sequence!('cats')
+Cat.create(name: "Moose", birth_date: "2009/1/1", sex: "M", color: "gray", description: "used to be a mean cat, but after hit by car he is nice")
+Cat.create(name: "Squish", birth_date: "2021/6/27", sex: "M", color: "black", description: "fluffy sassy boy cat")
+Cat.create(name: "Lilly", birth_date: "2017/3/1", sex: "F", color: "black", description: "Queen Bee")
+Cat.create(name: "Zoey", birth_date: "2017/3/1", sex: "F", color: "black", description: "weird but nice. Bites!")
+Cat.create(name: "Limbo", birth_date: "2016/3/17", sex: "M", color: "black", description: "seal X dolphin")
+Cat.create(name: "Jimmy", birth_date: "2023/4/15", sex: "M", color: "black", description: "Genius")
+
